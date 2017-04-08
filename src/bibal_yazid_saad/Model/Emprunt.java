d88f3bc9927f45de68c6f26d74a75a5f31c29f66
saad <saad.hamamama330@gmail.com>
2017-04-08 14:26:22 +0100

    Ajout Model Emprunt

---
+++ src/bibal_yazid_saad/Model/Emprunt.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibal_yazid_saad.Model;
+
+/**
+ *
+ * @author Asaad
+ */
+public class Emprunt {
+    
+}
