e5581b4afa5167c895a3616d61ddd65242242263
Dizay93 <elbadri.yazid@gmail.com>
2017-04-22 12:46:18 +0100

    MENU

--- src/bibal_yazid_saad/Model/Emprunt.java
+++ src/bibal_yazid_saad/Model/Emprunt.java
@@ -5,10 +5,79 @@
  */
 package bibal_yazid_saad.Model;
 
+import java.sql.Date;
+
 /**
  *
  * @author Asaad
  */
 public class Emprunt {
+    private int ID;
+    private int ExemplaireID;
+    private int UsagerID;
+    private Date Date_Emprunt;
+    private Date Date_Retour_Prevue;
+    private Date Date_Retour_Effectif;
+
+    public Emprunt(int ExemplaireID, int UsagerID, Date Date_Emprunt, Date Date_Retour_Prevue) {
+        this.ExemplaireID = ExemplaireID;
+        this.UsagerID = UsagerID;
+        this.Date_Emprunt = Date_Emprunt;
+        this.Date_Retour_Prevue = Date_Retour_Prevue;
+    }
+
+    public Emprunt(Date Date_Retour_Effectif) {
+        this.Date_Retour_Effectif = Date_Retour_Effectif;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getExemplaireID() {
+        return ExemplaireID;
+    }
+
+    public void setExemplaireID(int ExemplaireID) {
+        this.ExemplaireID = ExemplaireID;
+    }
+
+    public int getUsagerID() {
+        return UsagerID;
+    }
+
+    public void setUsagerID(int UsagerID) {
+        this.UsagerID = UsagerID;
+    }
+
+    public Date getDate_Emprunt() {
+        return Date_Emprunt;
+    }
+
+    public void setDate_Emprunt(Date Date_Emprunt) {
+        this.Date_Emprunt = Date_Emprunt;
+    }
+
+    public Date getDate_Retour_Prevue() {
+        return Date_Retour_Prevue;
+    }
+
+    public void setDate_Retour_Prevue(Date Date_Retour_Prevue) {
+        this.Date_Retour_Prevue = Date_Retour_Prevue;
+    }
+
+    public Date getDate_Retour_Effectif() {
+        return Date_Retour_Effectif;
+    }
+
+    public void setDate_Retour_Effectif(Date Date_Retour_Effectif) {
+        this.Date_Retour_Effectif = Date_Retour_Effectif;
+    }
+
+   
     
 }
